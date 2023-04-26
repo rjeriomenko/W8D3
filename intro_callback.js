da128d2a5740dba30b7745ea83e197814d44f6f9
Arvid Hossain <arvidh97@gmail.com>
2023-04-26 17:31:32 -0400

    add myBind

--- intro_callback.js
+++ intro_callback.js
@@ -37,6 +37,7 @@ class Clock {
 };
     
 // const clock = new Clock();
+function testAddNums() {
 
 const readline = require("readline");
 const rl = readline.createInterface({
@@ -59,3 +60,41 @@ function addNumbers(sum, numsLeft, completionCallback) {
 };
 
 addNumbers(0, 3, sum => console.log(`Total Sum: ${sum}`));
+};
+
+Function.prototype.myBind = function(context) {
+    return () => {
+        this.apply(context)
+    };
+};
+
+
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+  const turnOn = function() {
+    console.log("Turning on " + this.name);
+  };
+  
+  const lamp = new Lamp();
+  
+//   turnOn(); // should not work the way we want it to
+  
+  const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+  myBoundTurnOn(); // should say "Turning on a lamp
+    turnOn.myBind(lamp)(); // this is the same as myBoundTurnOn();
+
+class Lion {
+    constructor() {
+        this.name = "Leon";
+    };
+};
+
+const leon = new Lion();
+turnOn.myBind(leon)();
